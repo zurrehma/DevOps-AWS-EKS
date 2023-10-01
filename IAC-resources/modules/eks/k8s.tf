c7721688295309d3b666bef53bec8ebcd5f0391b
Loving Birds <lovingbirds@LHR-M-AHMAD.local>
2023-10-01 12:32:39 +0500

    update terraform remote state

--- IAC-resources/modules/eks/k8s.tf
+++ IAC-resources/modules/eks/k8s.tf
@@ -2,5 +2,14 @@ provider "kubernetes" {
   alias = "eks-cluster"
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
-
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "test"
+  }
+}
--- IAC-resources/modules/eks/outputs.tf
+++ IAC-resources/modules/eks/outputs.tf
@@ -1,14 +1,3 @@
-output "node_security_group_id" {
-  description = "ID of worker security group to be used in worker instances."
-  value       = module.eks.node_security_group_id
-}
-output "kubeconfig" {
-  value = templatefile("${path.module}/kubeconfig.tpl", {
-    cluster_endpoint                 = module.eks.cluster_endpoint
-    cluster_certificate_authority   = module.eks.cluster_certificate_authority_data
-  })
-}
-
 output "context" {
   description = "All outputs of module"
   value       = module.eks
