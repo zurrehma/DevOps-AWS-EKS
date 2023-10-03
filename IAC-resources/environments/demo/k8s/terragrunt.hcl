9c6c17a53e046c8748e214009a33e76772485fb2
Loving Birds <lovingbirds@LHR-M-AHMAD.local>
2023-10-03 21:13:20 +0500

    deploy new Environment

--- IAC-resources/environments/demo/k8s/terragrunt.hcl
+++ IAC-resources/environments/demo/k8s/terragrunt.hcl
@@ -10,8 +10,8 @@ dependency "eks"{
   config_path = "../eks"
 }
 
-inputs = {
-  cluster_endpoint = dependency.eks.aws_eks_cluster.cluster.endpoint
-  cluster_crt = dependency.eks.aws_eks_cluster.cluster.certificate_authority.0.data
-  cluster_token = dependency.eks.aws_eks_cluster_auth.cluster.token
-}
+# inputs = {
+#   cluster_endpoint = dependency.eks.aws_eks_cluster.cluster.endpoint
+#   cluster_crt = dependency.eks.aws_eks_cluster.cluster.certificate_authority.0.data
+#   cluster_token = dependency.eks.aws_eks_cluster_auth.cluster.token
+# }
--- IAC-resources/modules/kubernetes/k8s.tf
+++ IAC-resources/modules/kubernetes/k8s.tf
@@ -1,36 +1,20 @@
-# Wait for the EKS cluster to be created before proceeding
-resource "null_resource" "wait_for_eks" {
-  depends_on = [module.eks]
-  
-  # Use a local-exec provisioner to run a command that waits for the cluster to be ready
-  provisioner "local-exec" {
-    command = "sleep 60" # Replace with a command that checks EKS readiness
-  }
-}
-
 provider "kubernetes" {
   # alias = "eks-cluster"
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_crt)
-  token                  = var.cluster_token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 provider "helm" {
   kubernetes {
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_crt)
-    token                  = var.cluster_token
+    # alias = "eks-cluster"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
 
-# provider "helm" {
-#   kubernetes {
-#     # alias = "eks-cluster"
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#   }
-# }
+
 
 
 
