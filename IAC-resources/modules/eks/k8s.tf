8f73b8f064929b82f9982c88e825a0e1e54c5dd7
Loving Birds <lovingbirds@LHR-M-AHMAD.local>
2023-10-03 20:03:22 +0500

    deploy new Environment

--- IAC-resources/modules/eks/k8s.tf
+++ IAC-resources/modules/eks/k8s.tf
@@ -1,3 +1,13 @@
+# Wait for the EKS cluster to be created before proceeding
+resource "null_resource" "wait_for_eks" {
+  depends_on = [module.eks]
+  
+  # Use a local-exec provisioner to run a command that waits for the cluster to be ready
+  provisioner "local-exec" {
+    command = "sleep 60" # Replace with a command that checks EKS readiness
+  }
+}
+
 provider "kubernetes" {
   # alias = "eks-cluster"
   host                   = data.aws_eks_cluster.cluster.endpoint
