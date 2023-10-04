2b2115ac4931ab0d749f934eceecda3c2693fac2
Loving Birds <lovingbirds@LHR-M-AHMAD.local>
2023-10-04 11:26:32 +0500

    Fix Terragrunt issues

--- IAC-resources/modules/eks/eks-cluster.tf
+++ IAC-resources/modules/eks/eks-cluster.tf
@@ -128,15 +128,15 @@ resource "aws_security_group_rule" "allow_all_ingress" {
   security_group_id = module.eks.node_security_group_id
 }
 
-resource "aws_security_group_rule" "allow_all_egress" {
-  description       = "Allow all outbound traffic"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.eks.node_security_group_id
-}
+# resource "aws_security_group_rule" "allow_all_egress" {
+#   description       = "Allow all outbound traffic"
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = module.eks.node_security_group_id
+# }
 
 # resource "aws_iam_policy_attachment" "eks_policy_attachment" {
 #   name       = "${var.namespace}-${var.environment}-policy-attachment"
