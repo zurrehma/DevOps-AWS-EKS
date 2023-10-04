9385bfc496fcc0dd28114008d761223edcb4ade2
Loving Birds <lovingbirds@LHR-M-AHMAD.local>
2023-10-04 09:53:39 +0500

    Fix Terragrunt issues

--- IAC-resources/modules/eks/eks-cluster.tf
+++ IAC-resources/modules/eks/eks-cluster.tf
@@ -33,6 +33,7 @@ module "eks" {
   subnet_ids      = var.subnets
   vpc_id          = var.vpc_id
   iam_role_name   = "${var.namespace}-${var.environment}-role"
+  iam_role_use_name_prefix = true
   eks_managed_node_group_defaults = {
     ami_type       = var.ami_type
     instance_types = var.worker_group_instance_type
@@ -72,6 +73,9 @@ module "eks" {
       desired_size = var.autoscaling_group_desired_capacity
 
       instance_types = var.worker_group_instance_type
+      create_iam_role          = true
+      iam_role_name            = "${var.namespace}-${var.environment}-nodes-role"
+      iam_role_use_name_prefix = true
       iam_role_additional_policies = [
         "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
         "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
